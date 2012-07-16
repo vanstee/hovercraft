e4681b58328fd3a48981a9ef410b03f5c24339fd
vanstee <vanstee@highgroove.com>
2012-07-16 14:21:27 -0400

    Write helpers to deteremine desired format
    First try the format part of the query string like a conventional rails
    app. Then fall back to the mime type in the accept header and if
    anything goes default to json.

---
+++ lib/hovercraft/helpers.rb
@@ -0,0 +1,38 @@
+module Hovercraft
+  module Helpers
+    def self.registered(application)
+      application.helpers(Hovercraft::Helpers)
+    end
+
+    def respond_with(content)
+      if content.respond_to?(format_method_name)
+        content.send(format_method_name)
+      else
+        fail "Serialization method not supported: #{format_method_name}"
+      end
+    end
+
+    def format_method_name
+      "to_#{format}"
+    end
+
+    def format
+      params[:format] ||
+      preferred_type  ||
+      default_format
+    end
+
+    def preferred_type
+      case request.preferred_type
+      when /.*\/json/ then :json
+      when /.*\/xml/  then :xml
+      when /.*\/\*/   then default_format
+      else fail "Unknown media type: %p" % request.preferred_type
+      end
+    end
+
+    def default_format
+      :json
+    end
+  end
+end
